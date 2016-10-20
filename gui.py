97a1387bb6d02550f32f8f5d5de525079c19f160
LSChyi <alan81920@gmail.com>
2016-10-20 19:36:00 +0800

    add checkbox swtich instantaneously

--- gui.py
+++ gui.py
@@ -9,7 +9,9 @@ length = Entry(window)
 show_pass = IntVar()
 show_checkbox = Checkbutton(window, text="show password", variable=show_pass)
 password = Entry(window)
+password_str = ""
 def gen_pass():
+    global password_str
     length_int = length.get()
     length_int = 10 if length_int == '' else int(length_int)
     password_str = one_password.generate_pass(ori_pass.get(), service.get(), length_int)
@@ -20,6 +22,13 @@ def gen_pass():
         password.insert(END, "copied to clipboard")
     window.clipboard_clear()
     window.clipboard_append(password_str)
+
+def switch_show_pass(event):
+    password.delete(0, END)
+    if not show_pass.get():
+        password.insert(END, password_str)
+    else:
+        password.insert(END, "*" * len(password_str))
 
 Label(window, text="Easy remember passwod").pack()
 ori_pass.pack()
@@ -34,4 +43,5 @@ password.pack()
 ori_pass.bind("<Return>", lambda event: gen_pass())
 service.bind("<Return>", lambda event: gen_pass())
 length.bind("<Return>", lambda event: gen_pass())
+show_checkbox.bind("<Button-1>", switch_show_pass)
 window.mainloop()
