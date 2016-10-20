9a179968ce0d2321eee10b7a67f67e73cd33a340
LSChyi <alan81920@gmail.com>
2016-10-20 15:02:35 +0800

    change generate procedure into a function

--- one_password.py
+++ one_password.py
@@ -2,11 +2,15 @@ import base64
 import hashlib
 import getpass
 
-print("Enter easy remember password")
-original_pass = getpass.getpass()
-service = input("Enter service name: ")
-digits = input("Enter length or leave blank using default (10): ")
-digits = 10 if digits == '' else int(digits)
-hash_obj = hashlib.sha256(str.encode(original_pass + service))
-base64_pass = base64.b64encode(hash_obj.digest())
-print("Password: {}".format(base64_pass.decode("ascii")[:digits]))
+def generate_pass(original_pass, service):
+    hash_obj = hashlib.sha256(str.encode(original_pass + service))
+    base64_pass = base64.b64encode(hash_obj.digest())
+    return base64_pass.decode("ascii")[:digits]
+
+if __name__ == "__main__":
+    print("Enter easy remember password")
+    original_pass = getpass.getpass()
+    service = input("Enter service name: ")
+    digits = input("Enter length or leave blank using default (10): ")
+    digits = 10 if digits == '' else int(digits)
+    print("Password: {}".format(generate_pass(original_pass, service)))
