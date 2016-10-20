c79f7e4e2e62883b91ab8ce72f162f289cad7efa
LSChyi <alan81920@gmail.com>
2016-10-20 19:26:02 +0800

    fix name

--- one_password.py
+++ one_password.py
@@ -2,15 +2,15 @@ import base64
 import hashlib
 import getpass
 
-def generate_pass(original_pass, service, digits):
+def generate_pass(original_pass, service, length):
     hash_obj = hashlib.sha256(str.encode(original_pass + service))
     base64_pass = base64.b64encode(hash_obj.digest())
-    return base64_pass.decode("ascii")[:digits]
+    return base64_pass.decode("ascii")[:length]
 
 if __name__ == "__main__":
     print("Enter easy remember password")
     original_pass = getpass.getpass()
     service = input("Enter service name: ")
-    digits = input("Enter length or leave blank using default (10): ")
-    digits = 10 if digits == '' else int(digits)
-    print("Password: {}".format(generate_pass(original_pass, service, digits)))
+    length = input("Enter length or leave blank using default (10): ")
+    length = 10 if length == '' else int(length)
+    print("Password: {}".format(generate_pass(original_pass, service, length)))
